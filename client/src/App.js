fe5fa9270c5c57d17fccd0fa9356e21fd805faf7
Mohammad Ansar <ansar.ofcl00@gmail.com>
2022-12-02 19:20:14 +0530

    add theme

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,25 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
+import { createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "./theme";
 
 function App() {
+  let mode = useSelector((state) => state.mode);
+  let theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/profile/:userId" element={<ProfilePage />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </ThemeProvider>
     </div>
   );
 }
